a9a05356274f8e6bd898ee5b208ed192cd1ef10b
Dev6 <dev6@webteamwork>
2024-04-04 19:52:20 +0330

    change syntax error

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 function shadowM2(options) {
   let images = document.querySelectorAll(".shadowM2");
 
+
   if (options.shadow_type === "hard") {
     options.shadow_type = "0px";
-  } else options.shadowtype = "15px";
+  } else {
 
-  images.foreach(image=>{
-    image.style.boxshadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0,0.12)` 
+    options.shadow_type = "15px";
+  } 
+
+
+  images.forEach(image=>{
+    image.style.boxShadow  = `10px 10px ${options.shadow_type} black` 
 
     if (options.padding) {
+        image.style.margin = '1em'
         image.style.padding = '1em'
+        image.style.border = '1px solid black'
         
     }
   })
